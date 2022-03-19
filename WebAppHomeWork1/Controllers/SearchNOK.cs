e34e7aeee75041a2130eaf503f1aeacf1026a63b
KataevDmitriy <Kataev_Dmitriy@mail.ru>
2022-03-19 16:15:06 +0300

    add files

---
+++ WebAppHomeWork1/Controllers/SearchNOK.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAppHomeWork1.Models;
+namespace WebAppHomeWork1.Controllers
+{
+    public class SearchNOK : Controller
+    {
+        public IActionResult Index(int number1, int number2)
+        {
+            @ViewData["Title"] = "НОК";
+            ViewData["n1"] = number1;
+            ViewData["n2"] = number2;
+            
+            ulong result = 0;
+
+            SearchNOKModel sn = new SearchNOKModel(number1, number2);
+            result = sn.nok();
+            ViewData["res"] = result;
+            return View();
+        }
+    }
+}
